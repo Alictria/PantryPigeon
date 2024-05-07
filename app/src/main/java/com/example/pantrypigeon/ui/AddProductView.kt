129d248e672b78aa88ff56d8b6be686338f71cb0
Patricia Altorfer <patricia.altorfer@digitecgalaxus.ch>
2024-05-07 16:55:35 +0200

    Finish AddProductView look

--- app/src/main/java/com/example/pantrypigeon/ui/AddProductView.kt
+++ app/src/main/java/com/example/pantrypigeon/ui/AddProductView.kt
@@ -1,12 +1,32 @@
 package com.example.pantrypigeon.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,33 +35,106 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontVariation.width
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddProductScreen() {
     var productName by remember { mutableStateOf("") }
     var expirationDate by remember { mutableStateOf("") }
+    var pantryType by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.LightGray)
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Text(text = "Add Product")
+
+        val maxNameLength = 50
         OutlinedTextField(
+            modifier = Modifier.width(400.dp),
             value = productName,
-            onValueChange = { productName = it },
+            onValueChange = {
+                if (it.length <= maxNameLength) {
+                    productName = it
+                }
+            },
             label = { Text("Product name") }
         )
 
-        Row {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
             OutlinedTextField(
                 value = expirationDate,
                 onValueChange = { expirationDate = it },
-                label = { Text(text = "DD/MM/YYYY") }
+                label = { Text(text = "Date") },
+                modifier = Modifier.weight(1 / 3f)
+
             )
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            val context = LocalContext.current
+            val pantryTypes = listOf("Fridge", "Freezer", "Pantry")
+            var expanded by remember { mutableStateOf(false) }
+            var selectedText by remember { mutableStateOf(pantryTypes[0]) }
+
+            Box(
+                modifier = Modifier
+//                    .fillMaxWidth()
+                    .padding(4.dp)
+            ) {
+                /*ExposedDropdownMenuBox is experimental*/
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = {
+                        expanded = !expanded
+                    }
+                ) {
+                    TextField(
+                        value = selectedText,
+                        onValueChange = {},
+                        readOnly = true,
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                        modifier = Modifier.menuAnchor()
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        pantryTypes.forEach { item ->
+                            DropdownMenuItem(
+                                text = { Text(text = item) },
+                                onClick = {
+                                    selectedText = item
+                                    expanded = false
+                                    Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.End,
+    ) {
+        FloatingActionButton(
+            onClick = { },
+            Modifier.padding(16.dp)
+        ) {
+            Icon(Icons.Filled.Check, "Save item")
         }
     }
 }
