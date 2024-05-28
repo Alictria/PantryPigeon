6d14b07bbae2e9aaf9991b1fea13e3401bdee4bd
Patricia Altorfer <patricia.altorfer@digitecgalaxus.ch>
2024-05-28 10:54:48 +0200

    Update Add Product and Product Detail view to match Wireframe

--- app/src/main/java/com/example/pantrypigeon/ui/ProductDetailView.kt
+++ app/src/main/java/com/example/pantrypigeon/ui/ProductDetailView.kt
@@ -1,12 +1,10 @@
 package com.example.pantrypigeon.ui
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,7 +14,6 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -24,17 +21,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.pantrypigeon.Product
-import com.example.pantrypigeon.ProductEvent
-import com.example.pantrypigeon.ProductState
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 @Composable
 fun ProductDetailView(
     stateProductDetails: Product?
 ) {
-    val brush = Brush.horizontalGradient(listOf(Color.Red, Color.Blue))
+    val brush = Brush.horizontalGradient(listOf(Color.Green, Color.Blue))
     if (stateProductDetails != null) {
         Column(
             modifier = Modifier
@@ -43,6 +35,7 @@ fun ProductDetailView(
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
 
+            Text(text = "Product details", modifier = Modifier.padding(16.dp))
 
             Text(
                 text = stateProductDetails.productName, modifier = Modifier
@@ -51,9 +44,10 @@ fun ProductDetailView(
                     .padding(16.dp)
             )
 
+            Spacer(modifier = Modifier.padding(16.dp))
+
             Row(
                 modifier = Modifier
-                    .padding(16.dp)
                     .border(2.dp, brush, RoundedCornerShape(20))
                     .padding(16.dp)
                     .fillMaxWidth(),
@@ -72,18 +66,17 @@ fun ProductDetailView(
             }
 
         }
-    }
-    else {
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "No Product with this id", modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center
-        )
-    }
+    } else {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "No Product with this id", modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
